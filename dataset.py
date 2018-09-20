0e3ff7e0ddfbb77fb937a7acc4974c00b5f590cf
Liviu <liviurotiultelefon@gmail.com>
2018-09-20 13:54:10 +0300

    uncommented a line

--- dataset.py
+++ dataset.py
@@ -8,7 +8,7 @@ class Dataset():
     def __init__(self):
         self.image_name_list = get_image_names("/home/liviur/Documents/my_faster_rcnn/data/VOCdevkit/VOC2012/ImageSets/Main/aeroplane_train.txt")
         print("image names loaded")
-        #self.image_list = get_images(self.image_name_list, "/home/liviur/Documents/my_faster_rcnn/data/VOCdevkit/VOC2012/JPEGImages")
+        self.image_list = get_images(self.image_name_list, "/home/liviur/Documents/my_faster_rcnn/data/VOCdevkit/VOC2012/JPEGImages")
         print("images loaded")
         self.bbox_list = get_bbox_list(self.image_name_list, '/home/liviur/Documents/my_faster_rcnn/data/VOCdevkit/VOC2012/Annotations')
         print("bounding boxes laoded")
